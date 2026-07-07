6ff213939250c05d3302c9bb611dfb9d98e375ec
LimChanHyeok <ich053012@naver.com>
2026-07-07 20:22:07 +0900

    단위 테스트로 변경

--- src/test/java/org/example/community/CommunityApplicationTests.java
+++ src/test/java/org/example/community/CommunityApplicationTests.java
@@ -1,13 +1,14 @@
 package org.example.community;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Tag("integration")
 @SpringBootTest
 class CommunityApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
